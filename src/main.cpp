d6ec836e772d8d1770e9a3ee310418e840a07d3a
Teun Rensing <teun.rensing27@gmail.com>
2026-07-08 14:00:29 +0000

    Created working platformio basic program

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define LED_PIN 15
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("State: ON");
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("State: OFF");
+  delay(1000);
+}
